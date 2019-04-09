7ce6207528b814995b7b4902b5a93ed78a652da8
mvfsilva <mvfsillva@gmail.com>
2019-04-08 23:37:02 -0300

    feat: add log boilerplate

---
+++ index.js
@@ -0,0 +1,35 @@
+const winston = require('winston')
+
+const { config } = winston
+
+const formatNumber = num => ('0' + num).slice(-2)
+
+const date = new Date()
+const dd = formatNumber(date.getDate())
+const mm = formatNumber(date.getMonth() + 1)
+const yyyy = formatNumber(date.getFullYear())
+
+const h = formatNumber(date.getHours())
+const m = formatNumber(date.getMinutes())
+const s = formatNumber(date.getSeconds())
+
+const today = `${dd}/${mm}/${yyyy} ${h}:${m}:${s}`
+
+const messageTemplate = options => {
+  const level = config.addColors(options.level)
+  const { message = '' } = options
+
+  return `[${level} ${today}]: ${message}`
+}
+
+const hoopa = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.printf(options => {
+      return messageTemplate(options)
+    })
+  ),
+  transports: [new winston.transports.Console()],
+})
+
+module.exports = hoopa
