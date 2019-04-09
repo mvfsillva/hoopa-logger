92a5b07857533a86023a15d3ff1e07845112d1e7
mvfsilva <mvfsillva@gmail.com>
2019-04-09 02:55:03 -0300

    chore: add new examples

--- example/index.js
+++ example/index.js
@@ -1,5 +1,7 @@
-const hoopa = require ('../')
+const hoopa = require ('../.')
 
-hoopa.info('Hello World!')
-hoopa.warn('Attention guys!')
-hoopa.error('Gave some problems!')
+hoopa.info('This is information!')
+hoopa.warn('This is a warning!')
+hoopa.error('This is a problem!')
+hoopa.debug('This is a debug!')
+hoopa.verbose('This is a verbose!')
